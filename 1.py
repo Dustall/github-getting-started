6c106cd75e9dd80313e4389fd3d7613b5f3c2a6a
Nimaga <36283336+Dustall@users.noreply.github.com>
2024-08-21 17:23:20 +0000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,34 @@
+print("Session de connexion")
+
+ident = input("Entrer votre ID:  ")
+
+md = input("Entrer votre mot de passe:  ")
+
+
+
+if ident == "bouba" or md == "toto":
+    print("Bienvenue Monsieur ",ident)
+    chien = input("Quel est le nom de ton chien de compagnie ?: ")
+    print("Waou,",chien,"! ,C'est super comme nom")
+    race = input("Ton chien est de quelle race ?: ")
+    print("Oulala,",race,"est une superbe race!")
+else:
+    print("Identidfiant Incorrect")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3.py
@@ -0,0 +1,5 @@
+race_de_chien = ["golden retriever","chichawa","terrier","carlin"]
+
+for coco in race_de_chien:
+    print(coco)
+
